af9ed13efb3b6201c6dabe926aa68672cb41d9b3
Elmar Wiedemeijer <elmar.wiedemeijer@gmail.com>
2023-03-06 00:58:52 +0100

    feat: add blinking mode as test

--- source/clock.py
+++ source/clock.py
@@ -8,39 +8,46 @@ from luma.core.render import canvas
 from luma.core.virtual import viewport
 from luma.core.legacy import text, show_message
 from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
+from PIL import ImageFont
 
 
-def display(n, block_orientation, rotate, inreverse):
+def start_display(n, block_orientation, rotate, inreverse):
     # create matrix device
     serial = spi(port=0, device=0, gpio=noop())
     device = max7219(serial, cascaded=n or 1, block_orientation=block_orientation,
                      rotate=rotate or 0, blocks_arranged_in_reverse_order=inreverse)
-    print("Created device")
+    device.contrast(0)
+    return(device)
 
+def display(device, inverse):
     # start demo
-    device.contrast(255)
-    msg = "CLOCK"
-    print(msg)
-    show_message(device, msg, fill="white", font=proportional(SINCLAIR_FONT))
-
-    for intensity in range(0xFF,0,-1):
-        device.contrast(intensity)
-        time.sleep(0.01)
-    
-
+    if inverse:
+        outline = "white"
+        fill = "black"
+    else:
+        outline = "black"
+        fill = "white"
+#    show_message(device, text, fill="white", font=proportional(SINCLAIR_FONT))
+    with  canvas(device) as draw:
+        draw.rectangle(device.bounding_box, outline=outline, fill=fill)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='matrix_demo arguments',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument('--cascaded', '-n', type=int, default=4, help='Number of cascaded MAX7219 LED matrices')
-    parser.add_argument('--block-orientation', type=int, default=-90, choices=[0, 90, -90], help='Corrects block orientation when wired vertically')
+    parser.add_argument('--block-orientation', type=int, default=90, choices=[0, 90, -90], help='Corrects block orientation when wired vertically')
     parser.add_argument('--rotate', type=int, default=0, choices=[0, 1, 2, 3], help='Rotate display 0=0°, 1=90°, 2=180°, 3=270°')
-    parser.add_argument('--reverse-order', type=bool, default=False, help='Set to true if blocks are in reverse order')
+    parser.add_argument('--reverse-order', type=bool, default=True, help='Set to true if blocks are in reverse order')
 
     args = parser.parse_args()
 
     try:
-        display(args.cascaded, args.block_orientation, args.rotate, args.reverse_order)
+        device = start_display(args.cascaded, args.block_orientation, args.rotate, args.reverse_order)
+        while True:
+            display(device, True)
+            time.sleep(0.5)
+            display(device, False)
+            time.sleep(0.5)
     except KeyboardInterrupt:
         pass
