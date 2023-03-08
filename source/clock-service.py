5265b6f2119aad572a0f1d643ee65925ff9d4763
Elmar Wiedemeijer <elmar.wiedemeijer@gmail.com>
2023-03-08 08:52:58 +0100

    fix: single digit hours/minutes

--- source/clock-service.py
+++ source/clock-service.py
@@ -1,7 +1,8 @@
 import re
-import time
 import argparse
+import time
 
+from datetime import datetime
 from luma.led_matrix.device import max7219
 from luma.core.interface.serial import spi, noop
 from luma.core.render import canvas
@@ -9,7 +10,6 @@ from luma.core.virtual import viewport
 from luma.core.legacy import text, show_message
 from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
 from PIL import ImageFont
-
 
 def start_display(n, block_orientation, rotate, inreverse):
     # create matrix device
@@ -19,17 +19,28 @@ def start_display(n, block_orientation, rotate, inreverse):
     device.contrast(0)
     return(device)
 
-def display(device, inverse):
-    # start demo
-    if inverse:
-        outline = "white"
-        fill = "black"
-    else:
-        outline = "black"
-        fill = "white"
-#    show_message(device, text, fill="white", font=proportional(SINCLAIR_FONT))
+def display(device):
+    number_font = LCD_FONT
+    t = datetime.now().time()
+    hour = t.strftime("%H")
+    minute t.strftime("%M")
+    second = t.second
+
     with  canvas(device) as draw:
-        draw.rectangle(device.bounding_box, outline=outline, fill=fill)
+        text(draw, (2, 1), hour[0], fill="white", font=number_font)
+        text(draw, (8, 1), hour[1], fill="white", font=number_font)
+        text(draw, (19, 1), minute[0], fill="white", font=number_font)
+        text(draw, (25, 1), minute[1], fill="white", font=number_font)
+#        text(draw, (15, 0), ":", fill="white", font=number_font)
+
+#        eights = int(float(second)/60*8)
+#        draw.line([(31, 0), (31, eights)], fill="white")
+#        draw.line([(0,  0), (0,  eights)], fill="white")
+        second_binary = "{0:08b}".format(second)
+        for i, value in enumerate(second_binary):
+            if value == "1":
+                draw.point((15, 7-i), fill="white")
+                draw.point((16, 7-i), fill="white")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='matrix_demo arguments',
@@ -45,9 +56,7 @@ if __name__ == "__main__":
     try:
         device = start_display(args.cascaded, args.block_orientation, args.rotate, args.reverse_order)
         while True:
-            display(device, True)
-            time.sleep(0.5)
-            display(device, False)
-            time.sleep(0.5)
+            display(device)
+            time.sleep(1)
     except KeyboardInterrupt:
         pass
